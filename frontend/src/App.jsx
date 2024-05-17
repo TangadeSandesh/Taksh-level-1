6103119b4be70725f51d940061b1d6033e39a217
TangadeSandesh <tangadesandesh2001@gmail.com>
2024-05-17 23:36:10 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import react from 'react'
+import NavBar from './components/NavBar'
+import Guide from './components/Guide'
+
+
+function App() {
+  
+
+  return (
+    <div>
+        <NavBar/>
+        <Guide/>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+
+const NavBar = () => {
+  return (
+   <div>
+      <nav className="navbar navbar-light bg-dark">
+            <div className="container-fluid col-12">
+                <a className="navbar-brand badge text-dark col-md-6 " href="#">
+                <img src="" alt="" width="30" height="24" className="d-inline-block align-text-top"/>
+                    Khoj Taksh
+                </a>
+            </div>
+    </nav>
+    </div>
+  )
+};
+
+export default NavBar
